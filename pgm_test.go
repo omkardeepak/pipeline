7b4eddf0b522b3d4340713727451da449be43b5c
omkardeepak <omkardeepak444@gmail.com>
2025-12-09 23:19:25 +0530

    test done

--- pgm_test.go
+++ pgm_test.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
+	"testing"
 	"time"
 )
 
-func main() {
+func TestScript(t *testing.T) {
 	fmt.Println("waiting for test script...will take 3 seconds")
 	time.Sleep(3 * time.Second)
 	fmt.Println("Test script executed")
